bf6af143b6c96a9a099ef2a23adc5aaa6030c99a
robiulsagor <robiul100.me@gmail.com>
2025-07-19 18:13:57 +0600

    feat: Enhance ProductCard with hover effects and action buttons for cart, share, and like

--- client/app/components/ProductCard.tsx
+++ client/app/components/ProductCard.tsx
@@ -1,5 +1,13 @@
+'use client'
 import Image from "next/image"
 import ProductTag from "./ProductTag";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+
+import { GiShare } from "react-icons/gi";
+import { GoArrowSwitch, GoHeart } from "react-icons/go";
+import { toast } from "react-hot-toast";
 
 interface Product {
     id: number;
@@ -13,9 +21,62 @@ interface Product {
 }
 
 const ProductCard = ({ product }: { product: Product }) => {
+    const [isHovered, setIsHovered] = useState(false)
+
+    const addToCart = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        toast.success(`${product.name} added to cart!`);
+        console.log(`${product.name} added to cart!`);
+    }
+
+    const handleShare = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(window.location.href);
+        toast.success("Product link copied to clipboard!");
+    }
+
+    const handleLike = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        toast.success("Product liked!");
+    }
+
     return (
-        <div className="rounded-md overflow-hidden bg-[#f5f5f7] relative">
+        <Link href={'/'} className="rounded-md overflow-hidden bg-[#f5f5f7] relative"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             <ProductTag newProduct={product?.new || false} off={product?.off || false} />
+            <AnimatePresence>
+                {isHovered && (
+                    <motion.div
+                        key={product.id}
+                        initial={{ opacity: 0 }}
+                        whileInView={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                        className="absolute w-full h-full bg-[#3A3A3A]/80 left-0 top-0 flex flex-col items-center justify-center gap-8 text-white px-5">
+
+                        <button type="button" onClick={addToCart}
+                            className="bg-white text-primary hover:bg-slate-300 text-sm font-bold py-3 px-10 cursor-pointer transition">ADD TO CART</button>
+
+                        <div className="w-full flex items-center justify-between text-sm">
+                            <div className="inline-flex items-center gap-2" onClick={handleShare} >
+                                <GiShare className="text-xl cursor-pointer" />
+                                <span>Share</span>
+                            </div>
+                            <div className="inline-flex items-center gap-2">
+                                <GoArrowSwitch className="text-xl cursor-pointer" />
+                                <span>Compare</span>
+                            </div>
+                            <div className="inline-flex items-center gap-2" onClick={handleLike}>
+                                <GoHeart className="text-xl cursor-pointer" />
+                                <span>Like</span>
+                            </div>
+                        </div>
+                    </motion.div>
+                )}
+
+            </AnimatePresence>
+
             <Image src={product.image} alt={product.name} width={200} height={250} className="w-full h-64 " />
             <div className="p-4 flex flex-col gap-2">
                 <h3 className="text-2xl text-gray-700 font-semibold">{product.name}</h3>
@@ -31,7 +92,7 @@ const ProductCard = ({ product }: { product: Product }) => {
                     )}
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
